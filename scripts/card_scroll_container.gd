255f5b555ac16f21cdd75f0985ed8697ce4c2050
Noah <noahmosel73@gmail.com>
2023-11-04 15:43:32 -0500

    Card Viewer Scene

---
+++ scripts/card_scroll_container.gd
@@ -0,0 +1,19 @@
+extends ScrollContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#size = get_window().size
+	
+	#for basic_card in DirAccess.get_files_at("res://scenes/cards/basic/"):
+	#	var card = load("res://scenes/cards/basic/"+basic_card).instantiate()
+	#	add_child(card)
+	
+	#add_child(pawn_card)
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
