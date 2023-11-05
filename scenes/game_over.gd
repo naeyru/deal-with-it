e3be99a88ed2a370d64cc6f08b1fa4ea0f1a20fe
Thomas Safago <diskfull@protonmail.com>
2023-11-05 00:03:55 -0500

    Quick push

--- scenes/game_over.gd
+++ scenes/game_over.gd
@@ -1,20 +1,19 @@
-extends Button
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func _on_start_button_pressed():
+
+func _on_play_again_pressed():
 	get_tree().change_scene_to_file("res://scenes/main_game.tscn")
 
-func _on_card_view_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/card_viewer.tscn")
 
-func _on_exit_button_pressed():
-	get_tree().quit()
+func _on_main_menu_pressed():
+	get_tree().change_scene_to_file("res://scenes/start_menu.tscn")
